3249e43835e6a85ff6cf958da13930a3d9eb0b36
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-04-01 00:00:46 +0200

    dodanie komentarzy

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+// stworzenie mapy i poziomów danych
 var map = L.map('mapid').setView([52.408, 16.934], 11);
-map.createPane('najwyższe');
-map.createPane('wysokie');
-map.createPane('średnie');
-map.createPane('niskie');
-map.createPane('najniższe');
+map.createPane('najwyższe'); map.createPane('wysokie'); map.createPane('średnie');
+map.createPane('niskie'); map.createPane('najniższe');
 map.getPane('najwyższe').style.zIndex = 35;
 map.getPane('wysokie').style.zIndex = 30;
 map.getPane('średnie').style.zIndex = 20;
 map.getPane('niskie').style.zIndex = 15;
 map.getPane('najniższe').style.zIndex = 10;
 
+// wczytanie podkładów oraz WMS
 var OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19, attribution: '&copy;<a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
     pane: 'najniższe'
@@ -23,7 +22,7 @@ var drzewa = L.tileLayer.wms('http://wms2.geopoz.poznan.pl/geoserver/srodowisko/
     pane: 'najniższe'
 })
 
-document.getElementById('dane').style.fontSize = "30px";
+// interakcje z warstwą osiedli
 function layer_hover(feature, layer) {
     var popupContent = "<b>Nazwa:</b> " + feature.properties.NAZWA +
     "<br><b>L. mieszkańców:</b> " + feature.properties.LICZBA_MIESZKANCOW;
@@ -39,25 +38,36 @@ function layer_hover(feature, layer) {
     layer.on('mouseout', function (e) {
         this.bringToBack()
         this.setStyle({color: 'blue', fillOpacity: 0.1});
-        document.getElementById('dane').innerHTML = "Tu będzie treść";
+        document.getElementById('dane').innerHTML = "Najedź na osiedle aby zobaczyć dane na jego temat";
     });
 }
 
+// ikona pomników przyrody
 var ikona = L.icon({
     iconUrl: 'pomnik_przyrody.png',
-    iconSize:     [20, 20], // width and height of the image in pixels
-    iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    iconSize: [20, 20],
+    iconAnchor: [12, 12],
+    popupAnchor: [0, 0]
 });
 
+// funkcje tworzące dwa rodzaje punktów dla pomników przyrody
 function pomniki_ikony(point, latlng) {
     return L.marker(latlng, {icon: ikona});
 }
-
 function pomniki_punkty(point, latlng) {
     return L.circleMarker(latlng, {radius: 1, color: '#043812'});
 }
 
+// popupy pomników przyrody
+function pomniki_popup(feature, layer){
+    var gatunek = feature.properties.gatunek;
+    if (gatunek == 'brak danych') {
+        var gatunek = "<span style='color:red'>brak danych</span>"
+    };
+    layer.bindPopup("<b>Rodzaj:</b> " + feature.properties.obiekt + "<br><b>Gatunek:</b> " + gatunek)
+}
+
+// dodawanie odpowiedniej ikony pomników w zależności od zoomu
 map.on('zoomend', function(ev){
     if (map.getZoom() > 15) {
         formy_ochrony.removeLayer(pomniki_pkt)
@@ -68,12 +78,13 @@ map.on('zoomend', function(ev){
     }
 });
 
+// WARSTWY
 var admin = L.geoJson(poznan_admin, {color: 'blue', fillOpacity: 0.1, weight: 2,
     pane: 'średnie',
     onEachFeature: layer_hover
 });
 var poznan = L.geoJson(poznan, {color: 'black', weight: 4, pane: 'najwyższe'}).addTo(map);
-var tereny_zielone = L.geoJson(tereny_zielone, {//color: 'green', weight: 0, fillOpacity: 1, pane: 'niskie',
+var tereny_zielone = L.geoJson(tereny_zielone, {
     onEachFeature: function(feature, layer) {
         var nazwa = feature.properties.NAZWA;
         if (nazwa == null) {
@@ -92,31 +103,43 @@ var tereny_zielone = L.geoJson(tereny_zielone, {//color: 'green', weight: 0, fil
                 {color: 'green', weight: 0, fillOpacity: 1, pane: 'niskie'};
     }
 });
-var pomniki_pkt = L.geoJson(pomniki, {pointToLayer: pomniki_punkty, pane: 'wysokie',
-onEachFeature: function(feature, layer){
-    layer.bindPopup("Rodzaj: " + feature.properties.obiekt + "<br>Gatunek: " + feature.properties.gatunek)
+var pomniki_pkt = L.geoJson(pomniki, {pointToLayer: pomniki_punkty, pane: 'wysokie', onEachFeature: pomniki_popup});
+var pomniki_icon = L.geoJson(pomniki, {pointToLayer: pomniki_ikony, pane: 'wysokie', onEachFeature: pomniki_popup});
+var rezerwaty = L.geoJson(rezerwaty, {color: '#4f9839', fillOpacity: 0.4, pane: 'wysokie',
+onEachFeature: function(feature, layer) {
+    layer.bindPopup("<b><center>Rezerwat</center><br>Nazwa:</b> " + feature.properties.nazwa);
 }
 });
-var pomniki_icon = L.geoJson(pomniki, {pointToLayer: pomniki_ikony, pane: 'wysokie',
-onEachFeature: function(feature, layer){
-    layer.bindPopup("Rodzaj: " + feature.properties.obiekt + "<br>Gatunek: " + feature.properties.gatunek)
+var natura2000 = L.geoJson(natura2000, {color: '#4ac626', fillOpacity: 0.4, pane: 'wysokie',
+onEachFeature: function(feature, layer) {
+    layer.bindPopup("<b><center>Obszar Natura 2000</center><br>Nazwa:</b> " + feature.properties.nazwa);
 }
 });
-var rezerwaty = L.geoJson(rezerwaty, {color: '#4f9839', fillOpacity: 0.4, pane: 'wysokie'});
-var natura2000 = L.geoJson(natura2000, {color: '#4ac626', fillOpacity: 0.4, pane: 'wysokie'});
-var obszary_krajobrazu = L.geoJson(obszary_krajobrazu, {color: '#82c338', fillOpacity: 0.4, pane: 'wysokie'});
-var uzytki_ekologiczne = L.geoJson(uzytki_ekologiczne, {color: '#bae537', fillOpacity: 0.4, pane: 'wysokie'});
+var obszary_krajobrazu = L.geoJson(obszary_krajobrazu, {color: '#82c338', fillOpacity: 0.4, pane: 'wysokie',
+onEachFeature: function(feature, layer) {
+    layer.bindPopup("<b><center>Obszar chronionego krajobrazu</center><br>Nazwa:</b> " + feature.properties.nazwa);
+}
+});
+var uzytki_ekologiczne = L.geoJson(uzytki_ekologiczne, {color: '#bae537', fillOpacity: 0.4, pane: 'wysokie',
+onEachFeature: function(feature, layer) {
+    layer.bindPopup("<b><center>Użytek ekologiczny</center><br>Nazwa:</b> " + feature.properties.nazwa);
+}
+});
+
+// stworzenie grupy warstw dla form ochrony przyrody
 var formy_ochrony = L.layerGroup([pomniki_pkt, rezerwaty, natura2000, obszary_krajobrazu, uzytki_ekologiczne]);
 
+// dodanie geokodera OSM
 var osmGeocoder = new L.Control.OSMGeocoder({text: 'Wyszukaj', placeholder: 'wpisz nazwę...', bounds: L.LatLngBounds(poznan),
                                             collapsed: true, position: 'topleft'}).addTo(map);
 
-
+// mapy podkładowe
 var baseMaps = {
     "OSM": OSM,
     "Satelita": mapbox
 };
 
+// warstwy z danymi
 var overlayMaps = {
     "Poznań": poznan,
     "osiedla": admin,
@@ -125,8 +148,10 @@ var overlayMaps = {
     "formy ochrony przyrody": formy_ochrony
 };
 
+// przycisk zmiany widoczności warstw
 var layer_control = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
+// obsługa przycisku info
 var czy_info = 1;
 document.getElementById("info_przycisk").addEventListener("click", function(){
     if (czy_info == 1) {
